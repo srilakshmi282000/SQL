0ad3b5f2b2913593b1727a631160e5ed7953e46e
srilakshmi282000 <103499094+srilakshmi282000@users.noreply.github.com>
2022-04-12 18:04:58 +0530

    Add files via upload

---
+++ sql_test.sql
@@ -0,0 +1,11 @@
+#create database internship;
+#create database hivemetastore;
+#CREATE USER 'hive' IDENTIFIED BY 'admin';
+#GRANT ALL PRIVILEGES ON hivemetastore.* TO 'hive'@'%';
+#FLUSH PRIVILEGES;
+#CREATE USER 'srilakshmi' IDENTIFIED BY 'sri@2000';
+#GRANT ALL PRIVILEGES ON internship.* TO 'srilakshmi'@'%';
+#FLUSH PRIVILEGES;
+#CREATE TABLE pet (name VARCHAR(20), owner VARCHAR(20), species VARCHAR(20), sex CHAR(1), birth DATE, death DATE);
+#INSERT INTO pet VALUES ('Puffball','Diane','hamster','f','1999-03-30',NULL);
+#select * from pet;
